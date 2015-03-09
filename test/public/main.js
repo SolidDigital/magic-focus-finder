bd0705c9d93dd2a107f3878395ff921b0bfe9600
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2015-03-09 12:24:38 -0700

    Patch version 1.0.8, Removing the need for a mutation observer shim in old browsers.

--- test/public/main.js
+++ test/public/main.js
@@ -1,19 +1,12 @@
 /*jslint browser: true*/
 require.config({
-    shim : {
-        mutationObserverPolyfill : {
-            deps: ['weakMapPolyfill']
-        }
-    },
     paths : {
         lodash : './bower_components/lodash/lodash',
-        classListPolyFill : './bower_components/classListPolyfill/classList',
-        weakMapPolyfill : './bower_components/WeakMap/WeakMap',
-        mutationObserverPolyfill : './bower_components/MutationObserver/MutationObserver'
+        classListPolyFill : './bower_components/classListPolyfill/classList'
     }
 });
 
-require(['./source/main', 'classListPolyFill', 'mutationObserverPolyfill'], function (magicFocusFinder) {
+require(['./source/main', 'classListPolyFill'], function (magicFocusFinder) {
     'use strict';
 
     window.magicFocusFinder  = magicFocusFinder;
