7c5a2e1c06a353e8604f3d1d5a7e31cf27c78f77
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-03-10 22:50:41 -0700

    adding bower components

---
+++ vendor/magic-focus-finder/README.md
@@ -0,0 +1,136 @@
+# Magic Focus Finder
+
+Intended to help keyboard navigation through html nodes.
+
+### This is currently not a complete readme, I wrote this for a project I am on, will flesh out the details this weekend with example page and Travis-ci integration.
+
+---
+---
+---
+
+
+This is heavily influenced by (and sort of a rip off of) this repo: [jquery.keyJumper](https://github.com/mbitto/jquery.keyJumper)
+
+The diff will be
+
+1. no jquery
+2. using mutation observers to know when new focusable elements are added / removed from the DOM.
+3. full custom event support so that it will work with custom elements and dom binding libraries seemless.
+3. customizable key mapping so that I can use it with TV remote controls.
+4. wrap in integration tests
+5. semvar and bower
+6. amd compliant
+7. full wiki and examples page
+
+---
+
+how I would like to use it in a AMD module:
+
+```javascript
+define(['./vendor/magic-focus-finder/source/index'], function(magicFocusFinder) {
+// Basically I want to initialize it with an options object then be able to call start, stop, and refresh etc.
+  magicFocusFinder
+    .configure(optionsObject)
+    .start();
+});
+```
+
+Options Object
+
+```javascript
+{
+  keymap : {},  //override the default browser keymap
+  focusableAttribute : '', //override the default data attribute used to denote focusability
+  focusedClass : '', // a focus class for non focusable elements
+  defaultFocusedElement : '', // a element reference, or a selector
+  container : '', //the container in which this thing lives, default to the document.,
+  eventNamespace : '', //custom namespace for events, default to 'magicFocusFinder'
+  overrideDirectionAttribute : 'focus-overrides',
+  captureFocusAttribute : 'capture-focus',
+  dynamicPositionAttribute : 'dynamic-position'
+}
+```
+
+## Events
+It will fire the following events.
+
+### Element level
+Implemented
+1. `losing-focus` before the element loses focus.
+1. `focus-lost` when the focus is lost on an element.
+1. `gaining-focus` before the next element gains focus.
+1. `focus-gained` when the element actually gains focus.
+
+Not Implemented:
+1. `focus` normal focus event
+2. `blur` normal blur event
+5. `magicFocusFinder:focus:from:<% direction %>` namespaced focus event telling you which direction the focus came  from
+6. `magicFocusFinder:blur:to:<% direction %>` namespaced blur event telling you which direction the element was blurred to.
+
+### Container level
+Implemented
+
+Not Implemented:
+1. `magicFocusFinder:elementAdded` event telling you a focusable element was added to the container.
+2. `magicFocusFinder:elementRemoved` event telling you a focusable element was removed from the container.
+
+
+##Methods
+
+### configure(options)
+configure the defaults, can be called at anytime to change the configuration
+```javascript
+magicFocusFinder.configure(options)
+```
+
+### getConfig()
+returns the current configuration
+```javascript
+magicFocusFinder.getConfig()
+```
+
+### start()
+starts the dang thing, if start is called before configure, then default options will be used.
+```javascript
+
+```
+
+### lock()
+locks it up
+```javascript
+
+```
+
+### refresh()
+will refresh the element mapping (should only be used if your browser does not support mutation observers)
+Otherwise it will watch the dom for new elements.
+```javascript
+
+```
+
+### setCurrent(element)
+set the current focused element, element ref or selector
+```javascript
+
+```
+
+### getCurrent()
+get the current focues element, element ref of selector
+```javascript
+
+```
+
+### move(direction)
+tell the focus to move in a direction
+```javascript
+
+```
+
+### destroy()
+destroy this thing and free up all memory
+```javascript
+
+```
+---
+
+Rawk.
---
+++ vendor/magic-focus-finder/source/main.js
@@ -0,0 +1,361 @@
+/*jslint browser: true*/
+define(['lodash'], function (_) {
+    'use strict';
+
+    return {
+        configure : configure,
+        getConfig : getConfig,
+        start : start,
+        setCurrent : setCurrent,
+        refresh : refresh,
+
+        private : {
+            config : {
+                keymap : [
+                    {
+                        direction : 'up',
+                        code : 38
+                    },
+                    {
+                        direction : 'down',
+                        code : 40
+                    },
+                    {
+                        direction : 'left',
+                        code : 37
+                    },
+                    {
+                        direction : 'right',
+                        code : 39
+                    },
+                    {
+                        direction : 'enter',
+                        code : 13
+                    }
+                ],
+                focusableAttribute : 'focusable',
+                defaultFocusedElement : null,
+                container : 'document',
+                eventNamespace : 'magicFocusFinder',
+                focusedClass : 'focused',
+                overrideDirectionAttribute : 'focus-overrides',
+                captureFocusAttribute : 'capture-focus',
+                dynamicPositionAttribute : 'dynamic-position'
+            },
+            canMove : true,
+            currentlyFocusedElement : null,
+            knownElements : [],
+                // Each element will get the following properties when registered:
+                // magicFocusFinderPosition = the elements position.
+                // magicFocusFinderDirectionOverrides = if the element had any direction overrides.
+            move : {
+                up : _moveUp,
+                down : _moveDown,
+                left : _moveLeft,
+                right : _moveRight,
+                enter : _fireEnter
+            },
+            domObserver : null
+        }
+    };
+
+    function configure(options) {
+        _.merge(this.private.config, options);
+
+        return this;
+    }
+
+    function getConfig() {
+        return this.private.config;
+    }
+
+    function start() {
+        if(this.private.config.defaultFocusedElement) {
+            this.setCurrent(this.private.config.defaultFocusedElement);
+        }
+
+        this.refresh();
+
+        _setupAndStartWatchingMutations.call(this);
+    }
+
+    function setCurrent(querySelector) {
+        var currentlyFocusedElement = this.private.currentlyFocusedElement,
+            element;
+
+        element = querySelector && querySelector.nodeName ? querySelector : document.querySelector(querySelector);
+
+        if(element) {
+            if(currentlyFocusedElement) {
+
+                _fireEvent(currentlyFocusedElement, 'losing-focus');
+                currentlyFocusedElement.classList.remove(this.private.config.focusedClass);
+                currentlyFocusedElement.blur();
+                _fireEvent(currentlyFocusedElement, 'focus-lost');
+            }
+
+
+            _fireEvent(element, 'gaining-focus');
+            element.classList.add(this.private.config.focusedClass);
+            element.focus();
+            _fireEvent(element, 'focus-gained');
+
+            this.private.currentlyFocusedElement = element;
+        }
+
+        return this;
+    }
+
+    function refresh() {
+        var container;
+
+        if(this.private.config.container === 'document') {
+            container = document;
+        } else {
+            container = document.querySelector(this.private.config.container);
+        }
+
+        _.once(_setupBodyKeypressListener.bind(this))();
+
+        this.private.knownElements = [];
+
+        [].forEach.call(container.querySelectorAll('['+ this.private.config.focusableAttribute +']'), _registerElement.bind(this));
+    }
+
+    function _eventManager(event) {
+        var mappedKey;
+
+        if(!this.private.canMove) {
+            return;
+        }
+
+        _recalculateDynamicElementPositions.call(this);
+
+        if(this.private.currentlyFocusedElement) {
+            mappedKey = _.findWhere(this.private.config.keymap, { code : event.keyCode });
+
+            if(mappedKey && this.private.currentlyFocusedElement.magicFocusFinderDirectionOverrides[mappedKey.direction]) {
+                this.setCurrent(this.private.currentlyFocusedElement.magicFocusFinderDirectionOverrides[mappedKey.direction]);
+            } else if(mappedKey) {
+                this.private.move[mappedKey.direction].call(this);
+            }
+        } else {
+            _setDefaultFocus.call(this);
+        }
+    }
+
+    function _setDefaultFocus() {
+        if(this.private.config.defaultFocusedElement) {
+            this.setCurrent(this.private.config.defaultFocusedElement);
+        } else {
+            this.setCurrent(_.first(this.private.knownElements));
+        }
+    }
+
+    function _registerElement(element) {
+        var computedStyle = window.getComputedStyle(element);
+
+        if(computedStyle.display === 'none' || computedStyle.visibility === 'hidden') {
+            return false;
+        }
+
+        element.magicFocusFinderPosition = _getPosition(element);
+        element.magicFocusFinderDirectionOverrides = _getDirectionOverrides.call(this, element);
+
+        if(element.hasAttribute(this.private.config.captureFocusAttribute)) {
+            this.setCurrent(element);
+        }
+
+        this.private.knownElements.push(element);
+    }
+
+    function _unregisterElement(element) {
+        this.private.knownElements = _.reject(this.private.knownElements, function(knownElement) {
+            return knownElement.isEqualNode(element);
+        });
+    }
+
+    function _getPosition(element) {
+        var boundingRect = element.getBoundingClientRect(),
+            centerX = Math.round(boundingRect.left + (boundingRect.width / 2)),
+            centerY = Math.round(boundingRect.top + (boundingRect.height / 2));
+
+        return {
+            // Top-left corner coords
+            x : boundingRect.left,
+            y : boundingRect.top,
+            // Outer top center coords
+            otx : centerX,
+            oty : boundingRect.top,
+            // Outer bottom center coords
+            obx : centerX,
+            oby : boundingRect.bottom,
+            // Outer left center coords
+            olx : boundingRect.left,
+            oly : centerY,
+            // Outer right center coords
+            orx : boundingRect.right,
+            ory : centerY
+        };
+    }
+
+    function _recalculateDynamicElementPositions() {
+        _.each(this.private.knownElements, function(knownElement) {
+            if(knownElement.hasAttribute(this.private.config.dynamicPositionAttribute)) {
+                knownElement.magicFocusFinderPosition = _getPosition(knownElement);
+            }
+        }, this);
+    }
+
+    function _moveUp() {
+        var closeElements = _findCloseElements.call(this, function(current, other){
+            return current.oty >= other.oby;
+        });
+
+        _activateClosest.call(this, closeElements, 'up', function(current, other){
+            return Math.sqrt(Math.pow(current.oty - other.oby, 2) + Math.pow(current.otx - other.obx, 2));
+        });
+    }
+
+    function _moveDown() {
+        var closeElements = _findCloseElements.call(this, function(current, other){
+            return current.oby <= other.oty;
+        });
+
+        _activateClosest.call(this, closeElements, 'down', function(current, other){
+            return Math.sqrt(Math.pow(current.obx - other.otx, 2) + Math.pow(current.oby - other.oty, 2));
+        });
+    }
+
+    function _moveLeft() {
+        var closeElements = _findCloseElements.call(this, function(current, other){
+            return current.olx >= other.orx;
+        });
+
+        _activateClosest.call(this, closeElements, 'left', function(current, other){
+            return Math.sqrt(Math.pow(current.olx - other.orx, 2) + Math.pow(current.oly - other.ory, 2));
+        });
+    }
+
+    function _moveRight() {
+        var closeElements = _findCloseElements.call(this, function(current, other){
+            return current.orx <= other.olx;
+        });
+
+        _activateClosest.call(this, closeElements, 'right', function(current, other){
+            return Math.sqrt(Math.pow(current.orx - other.olx, 2) + Math.pow(current.ory - other.oly, 2));
+        });
+    }
+
+    function _fireEnter() {
+        var event = new MouseEvent('click', {
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
+        });
+
+        this.private.currentlyFocusedElement.dispatchEvent(event);
+    }
+
+    function _getDirectionOverrides(element) {
+        if(element.hasAttribute(this.private.config.overrideDirectionAttribute)) {
+            return _.zipObject(['up', 'right', 'down', 'left'], _.map(element.getAttribute(this.private.config.overrideDirectionAttribute).split(' '), function(direction) {
+                return direction !== 'null' ? direction : null;
+            }));
+        } else {
+            return {};
+        }
+    }
+
+    function _findCloseElements(isClose) {
+        var currentElementsPosition = this.private.currentlyFocusedElement.magicFocusFinderPosition;
+
+        return _.filter(this.private.knownElements, function(element) {
+            var isCloseElement = isClose(currentElementsPosition, element.magicFocusFinderPosition);
+
+            return isCloseElement && !this.private.currentlyFocusedElement.isEqualNode(element);
+        }.bind(this));
+    }
+
+    function _activateClosest(closeElements, direction, getDistance) {
+        var closestElement,
+            closestDistance,
+            currentElementsPosition = this.private.currentlyFocusedElement.magicFocusFinderPosition;
+
+        // Find closest element within the close elements
+        _.each(closeElements, function(closeElement) {
+            var closeElementsPosition = closeElement.magicFocusFinderPosition,
+                currentDistance;
+
+            // Find distance between 2 elements
+            currentDistance = getDistance(currentElementsPosition, closeElementsPosition);
+
+            // Check if is the closest found yet
+            if(_.isUndefined(closestDistance) || currentDistance < closestDistance) {
+                closestDistance = currentDistance;
+                closestElement = closeElement;
+            }
+
+            if(currentDistance === 0) {
+                return false; // exit early for speed.
+            }
+        });
+
+        if(closestElement){
+            this.setCurrent(closestElement);
+        }
+    }
+
+    function _setupBodyKeypressListener() {
+        document.querySelector('body').addEventListener('keydown', _eventManager.bind(this));
+    }
+
+    function _setupAndStartWatchingMutations() {
+        var self = this;
+
+        // Home baked mutation observer. The shim was VERY slow. This is much faster.
+        if(window.MutationObserver || window.WebKitMutationObserver) {
+            this.private.domObserver = new MutationObserver(function(mutations) {
+                mutations.forEach(function(mutation) {
+                    _.each(mutation.addedNodes, function(addedNode) {
+                        if(addedNode.hasAttribute(self.private.config.focusableAttribute) && addedNode.nodeName !== '#comment') {
+                            _registerElement.call(self, addedNode);
+                        }
+                    });
+
+                    _.each(mutation.removedNodes, function(removedNode) {
+                        if(removedNode.hasAttribute(self.private.config.focusableAttribute)) {
+                            _unregisterElement.call(self, removedNode);
+                        }
+                        if(self.private.currentlyFocusedElement.isEqualNode(removedNode)) {
+                            _setDefaultFocus.call(self);
+                        }
+                    });
+                });
+            });
+
+            this.private.domObserver.observe(document, { childList: true, subtree : true });
+        } else {
+            document.querySelector('body').addEventListener('DOMNodeInserted', function(event) {
+                if(event.target.hasAttribute(self.private.config.focusableAttribute) && event.target.nodeName !== '#comment') {
+                    _registerElement.call(self, event.target);
+                }
+            });
+
+            document.querySelector('body').addEventListener('DOMNodeRemoved', function(event) {
+                if(event.target.hasAttribute(self.private.config.focusableAttribute) && event.target.nodeName !== '#comment') {
+                    _unregisterElement.call(self, event.target);
+                }
+                if(self.private.currentlyFocusedElement.isEqualNode(event.target)) {
+                    _setDefaultFocus.call(self);
+                }
+            });
+        }
+    }
+
+    function _fireEvent(element, eventName) {
+        var event = document.createEvent('Event');
+        event.initEvent(eventName, true, true);
+        element.dispatchEvent(event);
+    }
+});
